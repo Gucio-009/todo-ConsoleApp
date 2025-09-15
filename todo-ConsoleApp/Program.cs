c9b29b523f97c641de9e08b42a10c8d8cc6ee916
Michał Gutowski <mgutowski2003@gmail.com>
2025-09-15 19:38:11 +0200

    Adding class, and most of his function

--- todo-ConsoleApp/Program.cs
+++ todo-ConsoleApp/Program.cs
@@ -1,10 +1,114 @@
-﻿namespace todo_ConsoleApp
+﻿using System.ComponentModel;
+
+namespace todo_ConsoleApp
 {
-    internal class Program
+    static class Program
     {
+        static List<Task> tasks = new List<Task>();
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            AddTask();
+            AddTask();
+            ShowTasks();
+
+            
+            
+
+        }
+
+        static public void AddTask()
+        {
+            Console.WriteLine("Podaj nazwe");
+            string name = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                Console.WriteLine("Zadanie nie może być puste!");
+                return;
+            }
+
+            Console.WriteLine("Podaj opis");
+            string description = Console.ReadLine() ?? "brak";
+            if (string.IsNullOrWhiteSpace(description))
+                description = "brak";
+            
+
+            tasks.Add(new Task(name, description));
+            Console.WriteLine("Pomyślnie dodano zadanie");
+
+        }
+
+        static public void SetAsDone()
+        {
+            if (tasks.Count != 0)
+            {
+                Console.WriteLine("Podaj id");
+                if (int.TryParse(Console.ReadLine(), out int id))
+                {
+                    var task = tasks.Find(t => t.id == id);
+                    if (task != null)
+                    {
+                        if(task.isCompleted)
+                            task.isCompleted = false;
+                        else
+                            task.isCompleted = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nie znaleziono zadania o takim ID.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Nieprawidłowe id");
+                }
+            }
+            else
+                Console.WriteLine("Brak zadań do zrobienia");
+        }
+
+        static public void DeleteTask()
+        {
+            if(tasks.Count != 0)
+            {
+                Console.WriteLine("Podaj id");
+                if (int.TryParse(Console.ReadLine(), out int id))
+                {
+                    var task = tasks.Find(t => t.id == id);
+                    if (task != null)
+                    {
+                        tasks.Remove(task);
+                        Console.WriteLine("Zadanie usunięte.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nie znaleziono zadania o takim ID.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Nieprawidłowe id");
+                }
+            }
+            else
+                Console.WriteLine("Brak zadań do usunięcia");
+
+        }
+
+        static public void ShowTasks()
+        {
+            foreach (var task in tasks)
+            {
+                if (task.isCompleted == false)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    task.GetInfo();
+                }
+                else
+                    task.GetInfo();
+                Console.ForegroundColor= ConsoleColor.White;
+            }
         }
     }
 }
---
+++ todo-ConsoleApp/Task.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace todo_ConsoleApp
+{
+    class Task
+    {
+        public static int nextId = 1; // automatyczne id
+        public int id {get; set;}
+        public string title { get; set; }
+        public string description { get; set; }
+        //private DateTime? DueDate;
+        public bool isCompleted = false;
+
+        public Task(string title, string description)
+        {
+            this.id = nextId++;
+            this.title = title;
+            this.description = description;
+            /*this.DueDate = dueDate;*/
+        }
+
+        // Konstruktor pomocniczy - bez daty
+        /*public Task(string title, string description)
+            : this(title, description, null) // wywołuje główny konstruktor z dueDate = null
+        {
+            
+        }*/
+
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"{id}. {title} ({description}) {(isCompleted ? "[✓]" : "[ ]")} ");
+        }
+
+        /*public string GetDueDateString()
+        {
+            return DueDate.HasValue ? DueDate.Value.ToString("yyyy-MM-dd") : "Brak daty";
+        }*/
+    }
+}
